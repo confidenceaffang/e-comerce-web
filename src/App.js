ae160775f662802e45647d5c628ebc156d39dab2
confidenceaffang <coaffa01@wsc.edu>
2024-09-30 16:48:12 -0500

    removed the logo, Apptest.js and added  components and imported them into App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "../src/components/Navbar";
+import Body1 from "./components/Body1.jsx";
+import Cards from "./components/Cards";
+import Analytics from "./components/Analtytics.jsx";
+import Footer from "./components/Footer.jsx";
+import Newsletter from "./components/Newsletter.jsx"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Body1 />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
